87e85c3250b5d0021bbaa4349aa96a2b90dfcbb3
edutco <edut97@gmail.com>
2020-11-15 17:43:16 +0200

    commit before deleting android studio

--- app/src/main/java/com/example/payeat/MenuByTitleActivity.java
+++ app/src/main/java/com/example/payeat/MenuByTitleActivity.java
@@ -3,12 +3,25 @@ package com.example.payeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MenuByTitleActivity extends AppCompatActivity {
 
+    ListView manuList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_by_title);
+        manuList=(ListView) findViewById(R.id.category_menu_list);
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("I");
+        arrayList.add("am");
+        arrayList.add("trying");
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        manuList.setAdapter(arrayAdapter);
     }
 }
