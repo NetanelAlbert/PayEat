919e3c998139e550bec4195fd47156c004ac304a
Eden.r <eden7657@gmail.com>
2020-11-23 19:26:09 +0200

    worked on MyCartActivity and activity_my_cart_list_item.xml

--- app/src/main/java/com/example/payeat/activities/MyCartActivity.java
+++ app/src/main/java/com/example/payeat/activities/MyCartActivity.java
@@ -2,7 +2,9 @@ package com.example.payeat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.payeat.R;
 
@@ -12,5 +14,18 @@ public class MyCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_cart);
+    }
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.order_after_viewing_cart_button :
+                intent = new Intent(this, BonAppetitActivity.class);
+                break;
+            default:
+
+
+        }
+        if(intent != null)
+            startActivity(intent);
     }
 }
