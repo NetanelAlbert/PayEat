4f267d4a583cc72bea1252abc0c967ef91260be4
Eden.r <eden7657@gmail.com>
2020-11-23 16:49:05 +0200

    moved to packeges

--- app/src/main/java/com/example/payeat/activities/ManagerLoginActivity.java
+++ app/src/main/java/com/example/payeat/activities/ManagerLoginActivity.java
@@ -2,9 +2,12 @@ package com.example.payeat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.payeat.R;
+import com.example.payeat.fragments.ChooseTableFragment;
 
 public class ManagerLoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class ManagerLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_login);
+    }
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.activity_manager_login_button :
+                intent = new Intent(this, ManagerOptionsActivity.class);
+                break;
+            default:
+
+
+        }
+        if(intent != null)
+            startActivity(intent);
     }
 }
