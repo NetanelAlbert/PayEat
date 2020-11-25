1d832ab0ed831501c91974872263129a32a6d0db
Ido Shapira <idshapira051@gmail.com>
2020-11-25 13:10:17 +0200

    Change edit dish from manager fragment

--- app/src/main/java/com/example/payeat/fragments/EditDishFromManagerFragment.java
+++ app/src/main/java/com/example/payeat/fragments/EditDishFromManagerFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.payeat.R;
@@ -18,12 +20,16 @@ import com.example.payeat.R;
  * Use the {@link EditDishFromManagerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditDishFromManagerFragment extends DialogFragment {
+public class EditDishFromManagerFragment extends DialogFragment implements View.OnClickListener {
 
     private static String DISH_NAME;
     private static String DISH_PRICE;
     private static String DISH_DESC;
     private static String DISH_IMAGE;
+
+    private EditText editText_new_name;
+    private EditText editText_new_price;
+    private EditText editText_new_desc;
 
     private String dish_name;
     private int dish_price;
@@ -73,6 +79,39 @@ public class EditDishFromManagerFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_dish_from_manager, container, false);
+        View convertView = inflater.inflate(R.layout.fragment_edit_dish_from_manager, container, false);
+
+        Button updateCostButton = convertView.findViewById(R.id.button_update_dish);
+        updateCostButton.setOnClickListener(this);
+
+        Button cancelCostButton = convertView.findViewById(R.id.button_cancel_update_dish);
+        cancelCostButton.setOnClickListener(this);
+
+        editText_new_name = convertView.findViewById(R.id.editText_update_dish_name);
+        editText_new_price = convertView.findViewById(R.id.editText_update_dish_cost);
+        editText_new_desc = convertView.findViewById(R.id.editText_update_dish_description);
+
+        editText_new_name.setText(dish_name);
+        editText_new_price.setText(""+dish_price);
+        editText_new_desc.setText(dish_desc);
+
+        return convertView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_update_dish:
+
+                dish_name = ""+editText_new_name.getText();
+                dish_price = Integer.parseInt(""+editText_new_price.getText());
+                dish_desc = ""+editText_new_desc.getText();
+                // send name,price,desc to the database
+                dismiss();
+                break;
+            case R.id.button_cancel_update_dish:
+                dismiss();
+                break;
+        }
     }
 }
