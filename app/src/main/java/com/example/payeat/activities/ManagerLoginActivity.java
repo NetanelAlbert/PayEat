db9b3e27d8ed0c007a6a1c87e404137ac926935d
edutco <edut97@gmail.com>
2020-12-07 16:34:52 +0200

    password is active after enter

--- app/src/main/java/com/example/payeat/activities/ManagerLoginActivity.java
+++ app/src/main/java/com/example/payeat/activities/ManagerLoginActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.payeat.Database;
@@ -24,6 +26,14 @@ public class ManagerLoginActivity extends AppCompatActivity implements  View.OnC
         goToManagerOptions = (Button) findViewById(R.id.activity_manager_login_button);
         goToManagerOptions.setOnClickListener((View.OnClickListener) this);
         password=(EditText) findViewById(R.id.manager_password_text);
+        password.requestFocus();
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                goToManagerOptions.callOnClick();
+                return true;
+            }
+        });
         //password.setText("password");
     }
     @Override
