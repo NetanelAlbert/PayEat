ffa97127181b775b011f630b9b5a53b8c280ef8c
Ido Shapira <idshapira051@gmail.com>
2020-12-16 21:40:54 +0200

    add option to change the logo, not yet update the database

--- app/src/main/java/com/example/payeat/activities/ManagerOptionsActivity.java
+++ app/src/main/java/com/example/payeat/activities/ManagerOptionsActivity.java
@@ -12,16 +12,25 @@ package com.example.payeat.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -73,6 +82,9 @@ public class ManagerOptionsActivity extends AppCompatActivity implements View.On
         return true;
     }
 
+    private static final int REQUEST_CAMERA=1001;
+    private static final int SELECT_FILE=1000;
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -81,12 +93,96 @@ public class ManagerOptionsActivity extends AppCompatActivity implements View.On
             case R.id.edit_restaurant_name:
                 return changeDetails("הכנס שם חדש למסעדה:", "שם מסעדה ריק!", Database.RESTAURANT_NAME);
             case R.id.edit_restaurant_logo:
-                // TODO
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //permission not granted for gallery, request it
+                    String[] permissions = {Manifest.permission.MANAGE_EXTERNAL_STORAGE};
+                    requestPermissions(permissions, REQUEST_CAMERA);
+                }
+                else if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    //permission not granted for camera, request it
+                    String[] permissions = {Manifest.permission.CAMERA};
+                    requestPermissions(permissions, SELECT_FILE);
+                }
+                else {
+                    //permission already granted
+                    selectImage();
+                }
                 return true;
+                // TODO update the database with the new image
+        }
+        return true;
+    }
+
+    private void selectImage() {
+        final CharSequence[] items= {"מצלמה", "גלריה" , "בטל"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ManagerOptionsActivity.this);
+        builder.setTitle("עדכן לוגו של המסעדה:");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(items[which].equals("מצלמה")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                }
+                else if(items[which].equals("גלריה")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "בחר תמונה") , SELECT_FILE);
+                }
+                else if(items[which].equals("בטל")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    selectImage();
+                }
+                else {
+                    Toast.makeText(this, "הרשאה לקבצים לא אושרה!" , Toast.LENGTH_SHORT);
+                }
+                break;
+            case SELECT_FILE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    selectImage();
+                }
+                else {
+                    Toast.makeText(this, "הרשאה לקבצים לא אושרה!" , Toast.LENGTH_SHORT);
+                }
+                break;
             default:
-                return super.onOptionsItemSelected(item);
+                selectImage();
+        }
+
+        selectImage();
+        System.out.println("here -->");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if(requestCode == REQUEST_CAMERA) {
+                final Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                imageViewRestaurantLogo.setImageBitmap(bitmap);
+            }
+            else if(requestCode == SELECT_FILE) {
+                Uri selectedImageUri = data.getData();
+                imageViewRestaurantLogo.setImageURI(selectedImageUri);
+            }
+            Toast.makeText(this, "Result ok", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Result canceled", Toast.LENGTH_LONG).show();
         }
     }
+
 
     private boolean changeDetails(String title, String onErrorMassage, final String whatToUpdate) {
         final Dialog edit_manager_name_dialog = new Dialog(this);
